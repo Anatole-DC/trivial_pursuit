02415b32e2a80123581e2a959dad32512ca947c3
Anatole-DC <adechauveron@gmail.com>
2023-01-03 13:06:31 +0100

    :bug: Fix database insert

--- frontend/lib/interface/screens/game/game_page.dart
+++ frontend/lib/interface/screens/game/game_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:swiping_card_deck/swiping_card_deck.dart';
 import 'package:trivial_pursuit/data/database/firebase_questions_repository.dart';
 import 'package:trivial_pursuit/data/models/game/list_questions.dart';
 import 'package:trivial_pursuit/interface/screens/game/bloc/game_cubit.dart';
@@ -25,20 +24,6 @@ class _GamePageState extends State<GamePage> {
     }
     return questionCards;
   }
-
-  // SwipingCardDeck questionsDeck(ListQuestions questions) {
-  //   List<Card> questionCards = [];
-  //   for (var question in questions.results) {
-  //     questionCards.add(Card(child: QuestionCard(question: question)));
-  //   }
-  //   return SwipingCardDeck(
-  //     cardWidth: 200,
-  //     onDeckEmpty: () => const Text("No more questions to awnser !"),
-  //     onLeftSwipe: (Card card) => debugPrint("Swiped left!"),
-  //     onRightSwipe: (Card card) => debugPrint("Swiped right!"),
-  //     cardDeck: questionCards,
-  //   );
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -67,52 +52,3 @@ class _GamePageState extends State<GamePage> {
             })));
   }
 }
-
-// return Center(
-//                         child: Column(
-//                       children: [
-//                         const SizedBox(
-//                           height: 50,
-//                         ),
-//                         Container(
-//                           constraints: const BoxConstraints(maxWidth: 275),
-//                           child: Text(
-//                             state.questions.results[0].question.toString(),
-//                             textAlign: TextAlign.center,
-//                             style: const TextStyle(
-//                                 fontSize: 40, fontWeight: FontWeight.bold),
-//                           ),
-//                         ),
-
-//                         // Awnsers
-//                         const SizedBox(
-//                           height: 75,
-//                         ),
-
-//                         Column(
-//                           children: [
-//                             Row(
-//                                 mainAxisAlignment: MainAxisAlignment.center,
-//                                 children: [
-//                                   AnwserButton(text: "1950"),
-//                                   const SizedBox(
-//                                     width: 75,
-//                                   ),
-//                                   AnwserButton(text: "1979")
-//                                 ]),
-//                             const SizedBox(
-//                               height: 40,
-//                             ),
-//                             Row(
-//                                 mainAxisAlignment: MainAxisAlignment.center,
-//                                 children: [
-//                                   AnwserButton(text: "2004"),
-//                                   const SizedBox(
-//                                     width: 75,
-//                                   ),
-//                                   AnwserButton(text: "1998")
-//                                 ])
-//                           ],
-//                         ),
-//                       ],
-//                     ));
